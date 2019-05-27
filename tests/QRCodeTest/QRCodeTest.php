606761dce7b1611c096aa44ba7df0bf2bcd6c0fc
Yakeing <yakeing@gmail.com>
2019-05-27 19:08:51 +0800

    Create QRCodeTest.php

---
+++ tests/QRCodeTest/QRCodeTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace QRCodeTest;
+use PHPUnit\Framework\TestCase;
+class QRCodeTest extends TestCase{
+  public function testQRCode(){
+  
+    $text = 'Test';
+    $pixel = 32;
+    $icon = false;
+    $distinguish ='L';
+    $type = 'png';
+    $margin = 0;
+    $color = 'FF0000|000000';
+    $stream = true;
+    $spec = 10;
+    $ret = qrcode::image($text, $pixel, $icon, $distinguish, $type, $margin, $color, $stream);
+
+    $this->assertTrue(is_string($ret));
+  }
+}
