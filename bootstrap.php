dafb24a6fd27d8a3f64f8897ceaddc7d5dd47145
Yakeing <yakeing@gmail.com>
2019-08-20 10:26:49 +0800

    Update and rename tests/bootstrap.php to bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+$loader = include __DIR__ . '/vendor/autoload.php';
+$loader->add('QRCode', __DIR__ . '/src');
--- tests/bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php
-$loader = include __DIR__ . '/../vendor/autoload.php';
-$loader->add('QRCode', __DIR__ . '/../src');
