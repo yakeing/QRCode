d61997613d6bd316dbe37ffe6f5c1a151ac1db00
Yakeing <yakeing@gmail.com>
2019-08-26 23:48:38 +0800

    Update QRCodeTest.php

--- tests/QRCodeTest.php
+++ tests/QRCodeTest.php
@@ -6,7 +6,7 @@ class QRCodeTest extends TestCase{
   public function testQRCode(){
     $text = 'Test';
     $pixel = 32;
-    $icon = false;
+    $icon = dirname(__FILE__).'icon.jpg';
     $distinguish ='L';
     $type = 'png';
     $margin = 0;
@@ -14,9 +14,8 @@ class QRCodeTest extends TestCase{
     $spec = 10;
     $str = QrCode::image($text, $pixel, $icon, $distinguish, $type, $margin, $color, $spec, true);
     $this->assertTrue(is_array($str));
-    $text = 1234567890;
-    $distinguish ='H';
-    $imag = QrCode::image($text, $pixel, $icon, $distinguish, $type, $margin, $color, $spec, false);
+    $text = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $imag = QrCode::image($text, $pixel, false, 'H', 'jpg', 2, $color, $spec, false);
     var_dump($imag);
     //$this->assertTrue(is_string($imag));
   }
